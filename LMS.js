99055865a062b227a15e87517ddc1037175c1bf4
Gaurav Singh <gaurav.singh@discoverdollar.com>
2022-09-02 07:55:17 +0000

    Created Add Book Functionality

--- LMS.js
+++ LMS.js
@@ -1,3 +1,28 @@
-class LMS {
-    
+
+let books = [];
+let deleted_books = [];
+
+function addBook(book_name,book_author,price,book_id,desc,genre){
+    if(book_name && book_author && price && book_id && desc && genre){
+        let book = {
+            book_name,book_author,price,book_id,desc,genre
+        }
+
+        let check = books.filter((val,id)=>{
+            return val.book_id === book_id;
+        })
+
+        if(check.length === 0){
+            books.push(book);
+            console.log("Book is Added Successfully")
+        }else{
+            console.error("Warning!! --> Book is Already Added")
+        }
+    }else{
+        console.error("Inappropriate Data");
+        console.log("Please Enter Data in Manner with Data- \n 'this.addBook(book_name,book_author,price,book_id,desc,genre)' ")
+    }
+
 }
+
+
